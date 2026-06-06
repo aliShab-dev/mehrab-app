6f20904252f15673a388a7d4d4c74b4b92d1f317
ali shabani <alishabanydev@gmail.com>
2026-06-06 13:42:59 +0330

    logos, manifast, metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,30 @@ export const metadata = {
   authors: [{ name: "استودیو محراب", url: "https://www.mehrabartmedia.ir" }],
   creator: "استودیو محراب",
   publisher: "استودیو محراب",
+
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+      },
+    ],
+  },
+
+  manifest: "/site.webmanifest",
+
   openGraph: {
     title: "استودیو محراب | رسانه و تولید هنری با رسالت و دغدغه",
     description:
@@ -30,6 +54,13 @@ export const metadata = {
     siteName: "استودیو محراب",
     locale: "fa_IR",
     type: "website",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 };
 
